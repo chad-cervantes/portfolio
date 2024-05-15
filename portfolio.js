a0c4d77b4023a7ee55c4b49fdfc47ceae190e1ca
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2024-05-14 21:22:00 -0600

    added javascript code for highlighting which navlink tab you're currently on

--- portfolio.js
+++ portfolio.js
@@ -4,3 +4,16 @@ hamburgerMenu.onclick = function() {
   navBar = document.querySelector(".navbar")
   navBar.classList.toggle("active")
 }
+
+// Highlight that you're on the current page
+const tabs = document.querySelectorAll('.tab');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    // Remove 'active' class from all tabs
+    tabs.forEach(t => t.classList.remove('active'));
+    
+    // Add 'active' class to the clicked tab
+    tab.classList.add('active');
+  });
+});
